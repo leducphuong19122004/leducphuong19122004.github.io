31fb00a720b26f88708a3f52a41b81e6af179d17
Le_Duc_Phuong <phuongtroc2004@gmail.com>
2023-04-22 15:27:39 +0700

    fix

--- logic.js
+++ logic.js
@@ -4,17 +4,21 @@ const menu_btn = document.querySelector(".menu");
 const menu_dropdown = document.querySelector('.menu-dropdown');
 const close_menu = document.querySelector('.close');
 
-menu_btn.addEventListener("click", () => {
-    menu_dropdown.style.display = "flex";
-    close_menu.style.display = "block";
-    menu_btn.style.display = "none";
-});
-
-close_menu.addEventListener("click", () => {
-    menu_dropdown.style.display = "none";
-    close_menu.style.display = "none";
-    menu_btn.style.display = "block";
-});
+window.onclick = (event) => {
+    if (event.target.matches('.close')) {
+        menu_dropdown.style.display = "none";
+        close_menu.style.display = "none";
+        menu_btn.style.display = "block";
+    } else if (event.target.matches('.menu')) {
+        menu_dropdown.style.display = "flex";
+        close_menu.style.display = "block";
+        menu_btn.style.display = "none";
+    } else {
+        menu_dropdown.style.display = "none";
+        close_menu.style.display = "none";
+        menu_btn.style.display = "block";
+    }
+};
 
 button_skill[0].addEventListener("click", () => {
     if (button_skill[0].textContent == "Read More") {
