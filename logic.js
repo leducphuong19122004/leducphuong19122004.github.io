97eccf22cbff660c6b1e73439b1dc8338a6fcb15
Le_Duc_Phuong <phuongtroc2004@gmail.com>
2023-04-22 15:01:42 +0700

    add reponsive and dropdown menu

--- logic.js
+++ logic.js
@@ -1,35 +1,47 @@
-const button_skill_1 = document.querySelector(".button_skill_1");
-const button_skill_2 = document.querySelector(".button_skill_2");
-const button_skill_3 = document.querySelector(".button_skill_3");
+const button_skill = document.querySelectorAll(".button_skill");
+const read_more_text = document.querySelectorAll(".read-more-text");
+const menu_btn = document.querySelector(".menu");
+const menu_dropdown = document.querySelector('.menu-dropdown');
+const close_menu = document.querySelector('.close');
 
-const text_skill_content = document.querySelectorAll(".text-skill-content");
+menu_btn.addEventListener("click", () => {
+    menu_dropdown.style.display = "flex";
+    close_menu.style.display = "block";
+    menu_btn.style.display = "none";
+});
 
-button_skill_2.addEventListener("click", () => {
-    if (button_skill_2.textContent == "See Less") {
-        text_skill_content[1].textContent = "My proficiency in backend development is unmatched, with a strong command of server-side programming languages like Node.js and C# and also with frameworks backend like Express and .NET, and...";
-        button_skill_2.textContent = "Read More";
+close_menu.addEventListener("click", () => {
+    menu_dropdown.style.display = "none";
+    close_menu.style.display = "none";
+    menu_btn.style.display = "block";
+});
+
+button_skill[0].addEventListener("click", () => {
+    if (button_skill[0].textContent == "Read More") {
+        read_more_text[0].style.display = "block";
+        button_skill[0].textContent = "Read Less";
     } else {
-        text_skill_content[1].textContent = "My proficiency in backend development is unmatched, with a strong command of server-side programming languages like Node.js and C# and also with frameworks backend like Express and .NET, and my ability to build scalable and robust server-side applications is unparalleled. I am always looking for ways to optimize performance and am constantly striving to improve the efficiency and reliability of my backend code."
-        button_skill_2.textContent = "See Less";
+        read_more_text[0].style.display = "none";
+        button_skill[0].textContent = "Read More";
     }
 });
 
-button_skill_1.addEventListener("click", () => {
-    if (button_skill_1.textContent == "See Less") {
-        text_skill_content[0].textContent = "I had experience in frontend development, with extensive experience in HTML, CSS, JavaScript, React and Vue. My ability to create beautiful and responsive user interfaces is unmatched...";
-        button_skill_2.textContent = "Read More";
+button_skill[1].addEventListener("click", () => {
+    if (button_skill[1].textContent == "Read More") {
+        read_more_text[1].style.display = "block";
+        button_skill[1].textContent = "Read Less";
     } else {
-        text_skill_content[0].textContent = "I had experience in frontend development, with extensive experience in HTML, CSS, JavaScript, React and Vue. My ability to create beautiful and responsive user interfaces is unmatched. I am constantly keeping up with the latest trends in frontend development and am always looking for new ways to improve my skills. "
-        button_skill_1.textContent = "See Less";
+        read_more_text[1].style.display = "none";
+        button_skill[1].textContent = "Read More";
     }
 });
 
-button_skill_3.addEventListener("click", () => {
-    if (button_skill_3.textContent == "See Less") {
-        text_skill_content[2].textContent = "My excellent communication and problem-solving skills allow me to collaborate effectively with my team and clients alike. I have a strong sense of empathy, which enables me to understand the needs of others and...";
-        button_skill_2.textContent = "Read More";
+button_skill[2].addEventListener("click", () => {
+    if (button_skill[2].textContent == "Read More") {
+        read_more_text[2].style.display = "block";
+        button_skill[2].textContent = "Read Less";
     } else {
-        text_skill_content[2].textContent = "My excellent communication and problem-solving skills allow me to collaborate effectively with my team and clients alike. I have a strong sense of empathy, which enables me to understand the needs of others and work towards finding solutions that meet their requirements. You are a natural leader who inspires and motivates those around you, and you have a knack for bringing out the best in people."
-        button_skill_3.textContent = "See Less";
+        read_more_text[2].style.display = "none";
+        button_skill[2].textContent = "Read More";
     }
 });
